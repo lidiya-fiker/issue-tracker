4444cd1e43343930f20b22fdcc0beb7fc3ccc0a4
lidiya-fiker <lidiyafikerr@gmail.com>
2024-12-17 21:02:57 +0300

    Add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,18 @@
-import { Button, Textarea, TextInput } from "@mantine/core";
-import React from "react";
+"use client";
+import { Button, TextInput } from "@mantine/core";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+
+
+const SimpleMDEEditor = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 function NewIssuePage() {
   return (
     <div className="max-w-xl space-y-3">
       <TextInput placeholder="Title" />
-      <Textarea placeholder="Description" />
+      <SimpleMDEEditor placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
